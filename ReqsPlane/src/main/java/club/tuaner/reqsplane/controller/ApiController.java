0a150efcdf602cb09ebfc423910364185817edc6
bear <booloose@163.com>
2021-11-20 01:09:30 +0800

    commit test

--- ReqsPlane/src/main/java/club/tuaner/reqsplane/controller/ApiController.java
+++ ReqsPlane/src/main/java/club/tuaner/reqsplane/controller/ApiController.java
@@ -1,12 +1,18 @@
 package club.tuaner.reqsplane.controller;
 
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    @RequestMapping(value = "/test/{value}",method = RequestMethod.GET)
+    public Object getHello(@PathVariable("value") String value){
+        return "hello  "+value;
+    }
 
 }
